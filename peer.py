574d6b283bfc87c5b1b45ebffaf6076d066fc43f
Omri Fridman <fomri2009@gmail.com>
2026-08-17 21:49:35 +0300

    Added request file function.

--- peer.py
+++ peer.py
@@ -30,6 +30,40 @@ class Peer:
         except Exception as e:
             print(f"[-] Failed to register to server: {e}")
 
+
+    def request_file(self, file_name):
+        """
+        asks the server for tell what peers hold the file and their information
+        """
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect((SERVER_IP, SERVER_PORT))
+                
+                request = {
+                    "action": "QUERY",
+                    "file_name": file_name
+                }
+                s.sendall(json.dumps(request).encode('utf-8'))
+
+                raw_response = s.recv(4096).decode('utf-8')
+                response = json.loads(raw_response)
+
+                if response.get("status") == "SUCCESS":
+                    peers = response.get("peers", [])
+                    print(f"[+] Found {len(peers)} peers holding '{file_name}':")
+                    for p in peers:
+                        print(f"    - IP: {p.get('ip')}, Port: {p.get('port')}, Bandwidth: {p.get('bandwidth')} Mbps")
+                    #from here we need to calculate allocation and then request for chunks.
+                    return peers
+                else:
+                    print(f"[-] File query failed: {response.get('message')}")
+                    return []
+
+        except Exception as e:
+            print(f"[-] Error querying file from server: {e}")
+            return []
+
+        
 if __name__ == "__main__":
     print("Welcome to ParallelPulse!")
     print("Registering with server...")
